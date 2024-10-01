aeab8b9c69accbbf9b481b5af5d2a8024a9b7543
fhaefeli <144069697+fhaefeli@users.noreply.github.com>
2024-09-30 21:31:13 -0300

    Add files via upload

---
+++ flask/seteo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Actualizar la lista de paquetes
+sudo apt-get update -y
+
+# Instalar Python y pip
+# sudo apt-get install python3 python3-pip -y
+sudo apt-get install python3 pip -y
+
+# Crear un entorno virtual
+# python3 -m venv venv
+virtualenv venv
+
+# Activar el entorno virtual
+source venv/bin/activate
+
+# Instalar dependencias necesarias
+pip install flask
+pip install numpy
+pip install scikit-learn
+
+# Mostrar el estado del repositorio git
+git status
+
+# Ejecutar el archivo main.py
+python main.py
